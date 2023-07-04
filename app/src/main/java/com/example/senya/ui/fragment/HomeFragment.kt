99889e14264d84b9612faa6488cc1ea19aa7b960
alex <loweab@gmail.com>
2023-07-04 16:05:46 -0400

    introduced viewbinding to project

--- app/src/main/java/com/example/senya/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/senya/ui/fragment/HomeFragment.kt
@@ -1,6 +1,37 @@
 package com.example.senya.ui.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.example.senya.R
+import com.example.senya.databinding.FrgmentHomeBinding
 
-class HomeFragment : Fragment() {
+class HomeFragment : BaseFragment() {
+
+    private var _binding: FrgmentHomeBinding? = null
+    private val binding
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FrgmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val recyclerView: RecyclerView = view.findViewById(R.id.recyclerView)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
