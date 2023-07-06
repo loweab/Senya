3216e5b0d1451c20ba519b2d4163c0e5a2ae975e
alex <loweab@gmail.com>
2023-07-06 17:17:49 -0400

    added ui for AttractionDetailFragment

--- app/src/main/java/com/example/senya/ui/fragment/AttractionDetailFragment.kt
+++ app/src/main/java/com/example/senya/ui/fragment/AttractionDetailFragment.kt
@@ -8,12 +8,14 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
+import com.example.senya.data.Attraction
 import com.example.senya.databinding.FragmentAttractionDetailBinding
 import com.example.senya.databinding.FrgmentHomeBinding
 import com.example.senya.ui.fragment.home.HomeFragmentAdapter
 import com.example.senya.ui.fragment.home.HomeFragmentDirections
+import com.squareup.picasso.Picasso
 
-class AttractionDetailFragment: Fragment() {
+class AttractionDetailFragment: BaseFragment() {
 
 
     private var _binding: FragmentAttractionDetailBinding? = null
@@ -21,6 +23,10 @@ class AttractionDetailFragment: Fragment() {
         get() = _binding!!
 
     private val safeArgs: AttractionDetailFragmentArgs by navArgs()
+
+    private val attraction: Attraction by lazy{
+        attractions.find { it.id == safeArgs.attractionId }!!
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -34,7 +40,16 @@ class AttractionDetailFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.button.text = safeArgs.attractionId
+        binding.apply {
+            titleTextView.text = attraction.title
+            descriptionTextView.text = attraction.description
+            Picasso.get().load(attraction.image_urls[0]).into(headerImageView);
+            monthsToVisitTextView.text = attraction.months_to_visit
+            numberOfFactsTextView.text = "${attraction.facts.size} facts"
+            binding.numberOfFactsTextView.setOnClickListener {
+                //todo
+            }
+        }
     }
 
     override fun onDestroyView() {
